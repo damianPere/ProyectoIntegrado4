730acf27a5777b9568c4c72e91a703281e25ae6c
Damian Perez <dajpes@gmail.com>
2025-03-11 18:58:40 -0500

    PI4-06: Se agrega un json faltanta del archivo test_transform

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -202,10 +202,7 @@ def test_query_orders_per_day_and_holidays_2017(database: Engine):
 
 
 def test_query_get_freight_value_weight_relationship(database: Engine):
-    # TODO -> Obtener el json de este test
-    # query_name = "get_freight_value_weight_relationship"
-    # actual: QueryResult = query_freight_value_weight_relationship(database)
-    # expected = read_query_result(query_name)
-    # print(pandas_to_json_object(actual.result))
-    # assert pandas_to_json_object(actual.result) == expected
-    assert True == True
+    query_name = "get_freight_value_weight_relationship"
+    actual: QueryResult = query_freight_value_weight_relationship(database)
+    expected = read_query_result(query_name)
+    assert pandas_to_json_object(actual.result) == expected
