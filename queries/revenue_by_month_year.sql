c462e79610c0e8c8342f9d0968e7185b508d9571
Damian Perez <dajpes@gmail.com>
2025-03-10 21:01:46 -0500

    PI4-05b: Se arregla query de revenue by month and year

--- queries/revenue_by_month_year.sql
+++ queries/revenue_by_month_year.sql
@@ -14,38 +14,51 @@ WITH RECURSIVE month_numbers AS (
 ),
 monthly_payments AS (
   SELECT
+    customer_id,
+    o.order_id,
+    order_delivered_customer_date,
+    order_status,
     strftime('%m', o.order_delivered_customer_date) AS delivery_month,
     strftime('%Y', o.order_delivered_customer_date) AS delivery_year,
-    SUM(p.payment_value) AS total_payment
+    p.payment_value AS total_payment
   FROM olist_orders o
   JOIN olist_order_payments p ON o.order_id = p.order_id
   WHERE o.order_status = 'delivered'
     AND o.order_delivered_customer_date IS NOT NULL
-    AND o.order_approved_at IS NOT NULL
+  GROUP BY customer_id, o.order_id, order_delivered_customer_date,
+           delivery_month, delivery_year
+  ORDER BY order_delivered_customer_date ASC
+),
+yearly_monthly_totals AS (
+  SELECT
+    delivery_month,
+    delivery_year,
+    SUM(total_payment) AS total_payment
+  FROM monthly_payments
   GROUP BY delivery_month, delivery_year
 )
 SELECT
     mn.month_no,
     CASE mn.month_no
-        WHEN '01' THEN 'Jan'
+        WHEN '01' THEN 'Ene'
         WHEN '02' THEN 'Feb'
         WHEN '03' THEN 'Mar'
-        WHEN '04' THEN 'Apr'
+        WHEN '04' THEN 'Abr'
         WHEN '05' THEN 'May'
         WHEN '06' THEN 'Jun'
         WHEN '07' THEN 'Jul'
-        WHEN '08' THEN 'Aug'
+        WHEN '08' THEN 'Ago'
         WHEN '09' THEN 'Sep'
         WHEN '10' THEN 'Oct'
         WHEN '11' THEN 'Nov'
-        WHEN '12' THEN 'Dec'
+        WHEN '12' THEN 'Dic'
     END AS month,
-    printf('%.2f', COALESCE(SUM(CASE WHEN mp.delivery_year = '2016' THEN mp.total_payment ELSE 0 END), 0.0)) AS Year2016,
-    printf('%.2f', COALESCE(SUM(CASE WHEN mp.delivery_year = '2017' THEN mp.total_payment ELSE 0 END), 0.0)) AS Year2017,
-    printf('%.2f', COALESCE(SUM(CASE WHEN mp.delivery_year = '2018' THEN mp.total_payment ELSE 0 END), 0.0)) AS Year2018
+    printf('%.2f', COALESCE(SUM(CASE WHEN ymt.delivery_year = '2016' THEN ymt.total_payment ELSE 0 END), 0.0)) AS Year2016,
+    printf('%.2f', COALESCE(SUM(CASE WHEN ymt.delivery_year = '2017' THEN ymt.total_payment ELSE 0 END), 0.0)) AS Year2017,
+    printf('%.2f', COALESCE(SUM(CASE WHEN ymt.delivery_year = '2018' THEN ymt.total_payment ELSE 0 END), 0.0)) AS Year2018
 FROM
     month_numbers mn
-LEFT JOIN monthly_payments mp ON mp.delivery_month = mn.month_no
+LEFT JOIN yearly_monthly_totals ymt ON ymt.delivery_month = mn.month_no
 GROUP BY
     mn.month_no
 ORDER BY
